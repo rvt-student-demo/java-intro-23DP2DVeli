3534f7c5bb1a8c1397a7b9f0bcea4c99a3ae8744
23DP2DVeli <166109636+23DP2DVeli@users.noreply.github.com>
2024-09-12 10:22:24 +0000

    Speeding Ticket

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,14 +6,11 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give the first number:");
-        int first = Integer.valueOf(scanner.nextLine());
-        System.out.println("Give the second number:");
-        int second = Integer.valueOf(scanner.nextLine());
-        System.out.println(first + " + " + second + " = " + (first + second));
-        System.out.println(first + " - " + second + " = " + (first - second));
-        System.out.println(first + " * " + second + " = " + (first * second));
-        System.out.println(first + " / " + second + " = " + (1.0 * first / second));
+        System.out.println("Give speed!");
+        int number = Integer.valueOf(scanner.nextLine());
+        if (number > 120) {
+            System.out.println("Speeding ticket!");
+        }
 
 
     }
