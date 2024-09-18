a956c11a75e2d10ce557e0cc13ec8e423bb96013
23DP2DVeli <166109636+23DP2DVeli@users.noreply.github.com>
2024-09-18 10:29:35 +0000

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,13 +6,25 @@ public class App
     {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Give speed!");
+        System.out.println();
         int number = Integer.valueOf(scanner.nextLine());
-        if (number > 120) {
-            System.out.println("Speeding ticket!");
+        if (number < 0) {
+            System.out.println("impossible!");
+        } else if (number <= 49) {
+            System.out.println("Grade: failed");
+        } else if (number >= 50 && number <= 59 ) {
+            System.out.println("Grade: 1");            
+        } else if (number >= 60 && number <= 69) {
+            System.out.println("Grade: 2");
+        } else if (number >= 70 && number <= 79) {
+            System.out.println("Grade: 3");
+        } else if (number >= 80 && number <= 89) {
+            System.out.println("Grade: 4");
+        } else if (number >= 90 && number <= 100) {
+            System.out.println("Grade: 5");
+        } else if (number > 100) {
+            System.out.println("Grade: incredible!");
         }
-
-
     }
 }
 
