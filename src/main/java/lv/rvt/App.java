08374aa0a1f0f5988d14b29237c25d879537afe3
23DP2DVeli <166109636+23DP2DVeli@users.noreply.github.com>
2024-09-19 10:24:29 +0000

    Only positives

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,20 +1,31 @@
 package lv.rvt;
 import java.util.Scanner;
+
+import javax.swing.plaf.TreeUI;
 public class App 
 {
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
 
-        
-
-        System.out.println("Value 1: ");
-        int value1 = Integer.valueOf(scanner.nextLine());
-        if (value1 < 0) {
-            int result = value1 * (-1);
-            System.out.println(result);
-        } else {
-            System.out.println(value1);
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine()); 
+            if (number < 0) {
+                System.out.println("Unsuitable number");
+                continue;
+            } 
+            
+            if (number ==  0) {
+                break;
+            }
+            
+    
+            if (number > 0) {
+                number = number * number;
+                System.out.println(number);
+                continue;
+            }
+            
         }
     }
 }
