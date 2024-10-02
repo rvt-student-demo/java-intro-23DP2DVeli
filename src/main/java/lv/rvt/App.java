e4beff52df8b318739481032ab24ad102a0d28a3
23DP2DVeli <166109636+23DP2DVeli@users.noreply.github.com>
2024-10-02 10:55:36 +0000

    From one to parameter

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,37 +7,17 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Give numbers: ");
-        int sum = 0;
-        int numbers = 0;
-        int count = 0;
-        int even = 0;
-        int odd = 0;
-        while (true) {
-            int input = Integer.valueOf(scanner.nextLine());
-            if (input == -1) {
-                System.out.println("Thx! Bye!");
-                break;
-            }
-            if (input < 0) {
-                count++;
-                continue;
-            } 
-            if (input % 2 == 0) {
-                even++;
-            } else {
-                odd++;
-            }
-            sum += input;
-            numbers++;
-            count++;      
-        }
-        System.out.println("Sum: " + sum);
-        System.out.println("Numbers: " + numbers);
-        System.out.println("Average: " + (1.0 * sum / count));   
-        System.out.println("Even: " + even);
-        System.out.println("Odd: " + odd);
+        int num = Integer.valueOf(scanner.nextLine());
+        printUntilNumber(5);
+        
     }
+
+    public static void printUntilNumber(int numOfTimes) {
+        int i = 1;
+        while (i <= numOfTimes) {
+            System.out.println(i++); 
+    }
+}
 }
 
 
