8ea4af3f46d5808b549099cb0b206a336bfa9f99
23DP2DVeli <166109636+23DP2DVeli@users.noreply.github.com>
2024-10-17 08:21:54 +0000

    Exercise - Guessing Game

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,32 +1,24 @@
 package lv.rvt;
 import java.util.ArrayList;
 import java.util.Scanner;
-
+import java.util.Random;
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        ArrayList<Integer> numbers = new ArrayList<>();
-        while (true) {
-            int input = scanner.nextInt();
-            if (input == 0) {
+        Random rand = new Random();
+        int num = rand.nextInt(10);
+        System.out.println("I am thinking of a number from 1 to 10. You must guess what it is in three tries.");
+        for (int i = 0; i < 3; i ++) {
+            System.out.println("Enter a guess:");
+            int x = Integer.valueOf(scanner.nextLine());
+            if (x == num) {
+                System.out.println("RIGHT!" );
                 break;
+            } else {
+                System.out.println("wrong" );
+                continue;
             }
-            numbers.add(input);
-        }
-        if (numbers.size() < 3) {
-            System.out.println("Not enough numbers in the list");
-        } else {
-            int sum = numbers.get(1) + numbers.get(2);
-            System.out.println(sum);
-        }
-        if (numbers.size() < 3) {
-            System.out.println("Not enough numbers in the list");
-        } else {
-            int summa_array = 0;
-            for (int i = 0; i < numbers.size(); i++) {
-                summa_array += numbers.get(i);
-            }
-            System.out.println(summa_array);
         }
     }
+
 }
