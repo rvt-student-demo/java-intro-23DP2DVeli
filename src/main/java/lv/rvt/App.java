b1c3d8757a08c88a4422ba790cdf7ba71aedb6e8
23DP2DVeli <166109636+23DP2DVeli@users.noreply.github.com>
2024-10-17 10:14:12 +0000

    Revised Guessing Game

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,25 +7,44 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         Random rand = new Random();
         int num = rand.nextInt(10);
+
+        int attempt = 5;
+
         System.out.println("I am thinking of a number from 1 to 10. You must guess what it is in three tries.");
-        for (int i = 0; i < 3; i ++) {
+        while (true) {
             System.out.println("Enter a guess:");
             int x = Integer.valueOf(scanner.nextLine());
-            if (x == num) {
-                System.out.println("RIGHT!" );
+            if (attempt == 1){
+                System.out.println("You lost.");
                 break;
-            
-            } else {
-                if (i == 2) {
-                System.out.println("wrong");
-                System.out.println("The correct number was " + num + ". You have lost the game.");
-                break;
-                }            
-            temperature(x, num);
-                
             }
-        }   
+
+
+            if (x == num){
+                System.out.println("You WON the game!");
+                System.out.println("Your score is " + attempt);
+                break;
+            }
+            else{
+                diff(x, num);
+                attempt--;
+                System.out.println("Your score is: " + attempt);
+            }
+
+        }
     }
+
+    public static void diff(int x, int num){
+            if (x < num) {
+                System.out.println("Too low.");
+            } else if (x > num){
+                System.out.println("Too high");
+            }
+
+            
+        }
+
+
     public static void temperature(int x, int num){
             if (x == num-3 || x == num+3) {
                 System.out.println("cold");
