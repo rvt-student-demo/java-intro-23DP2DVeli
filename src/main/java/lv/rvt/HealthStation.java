8d2ad393735a1b1f7b9c98a3213b835378fc5df9
23DP2DVeli <166109636+23DP2DVeli@users.noreply.github.com>
2024-11-28 11:23:34 +0000

    Health station : Counting weighings

--- src/main/java/lv/rvt/HealthStation.java
+++ src/main/java/lv/rvt/HealthStation.java
@@ -2,18 +2,25 @@ package lv.rvt;
 
 public class HealthStation {
     private int weight;
+    private int weighings;
     public void Health(Person person) {
         int weight = person.getWeight();
         this.weight = weight;
     }
     public int weigh(Person person) {
         this.weight = person.getWeight();
+        weighings++;
         return this.weight;
+
     }
 
     public void feed(Person person) {
         this.weight+=1;
         person.setWeight(person.getWeight() + 1);
         
+    }
+
+    public int weighings() {
+        return weighings;
     }
 }
